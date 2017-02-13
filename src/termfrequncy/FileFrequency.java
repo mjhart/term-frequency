517883be00399116c79a95ba92ae670529d80bdc
mjhart <matthew_hart@brown.edu>
2017-02-12 18:39:57 -0700

    remove all tabs

--- src/termfrequncy/FileFrequency.java
+++ src/termfrequncy/FileFrequency.java
@@ -3,33 +3,33 @@ package termfrequncy;
 import java.nio.file.Path;
 
 public class FileFrequency {
-	private final Path fileName;
-	private final double frequency;
-	
-	FileFrequency(Path filePath, double frequency) {
-		this.fileName = filePath;
-		this.frequency = frequency;
-	}
-	
-	public Path filePath() {
-		return fileName;
-	}
-	
-	public double frequency() {
-		return frequency;
-	}
-	
-	@Override
-	public boolean equals(Object other) {
-		if (other instanceof FileFrequency) {
-			FileFrequency otherFf = (FileFrequency) other;
-			return this.fileName == otherFf.fileName && this.frequency == otherFf.frequency;  
-		}
-		return false;
-	}
-	
-	@Override
-	public int hashCode() {
-		return fileName.hashCode() * 103 + Double.hashCode(frequency);
-	}
+  private final Path filePath;
+  private final double frequency;
+  
+  FileFrequency(Path filePath, double frequency) {
+    this.filePath = filePath;
+    this.frequency = frequency;
+  }
+  
+  public Path filePath() {
+    return filePath;
+  }
+  
+  public double frequency() {
+    return frequency;
+  }
+  
+  @Override
+  public boolean equals(Object other) {
+    if (other instanceof FileFrequency) {
+      FileFrequency otherFf = (FileFrequency) other;
+      return this.filePath == otherFf.filePath && this.frequency == otherFf.frequency;  
+    }
+    return false;
+  }
+  
+  @Override
+  public int hashCode() {
+    return filePath.hashCode() * 103 + Double.hashCode(frequency);
+  }
 }
--- src/termfrequncy/TermFrequency.java
+++ src/termfrequncy/TermFrequency.java
@@ -1,33 +1,33 @@
 package termfrequncy;
 
 public class TermFrequency {
-	private final String term;
-	private final double frequency;
-	
-	TermFrequency(String term, double frequency) {
-		this.term = term;
-		this.frequency = frequency;
-	}
-	
-	public String term() {
-		return term;
-	}
-	
-	public double frequency() {
-		return frequency;
-	}
-	
-	@Override
-	public boolean equals(Object other) {
-		if (other instanceof TermFrequency) {
-			TermFrequency otherTf = (TermFrequency) other;
-			return this.term == otherTf.term && this.frequency == otherTf.frequency;  
-		}
-		return false;
-	}
-	
-	@Override
-	public int hashCode() {
-		return term.hashCode() * 103 + Double.hashCode(frequency);
-	}
+  private final String term;
+  private final double frequency;
+  
+  TermFrequency(String term, double frequency) {
+    this.term = term;
+    this.frequency = frequency;
+  }
+  
+  public String term() {
+    return term;
+  }
+  
+  public double frequency() {
+    return frequency;
+  }
+  
+  @Override
+  public boolean equals(Object other) {
+    if (other instanceof TermFrequency) {
+      TermFrequency otherTf = (TermFrequency) other;
+      return this.term == otherTf.term && this.frequency == otherTf.frequency;  
+    }
+    return false;
+  }
+  
+  @Override
+  public int hashCode() {
+    return term.hashCode() * 103 + Double.hashCode(frequency);
+  }
 }
